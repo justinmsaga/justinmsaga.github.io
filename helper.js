1ee810d056dbdff66ad03fe89d6b8466f9969812
Justin Msaga <146983172+justinmsaga@users.noreply.github.com>
2025-12-13 16:07:41 -0800

    v5 of prorfolio home page

---
+++ helper.js
@@ -0,0 +1,110 @@
+//inputs(element type, text context, css styles to add, element id)
+function createElement(type, text, styles, name) {
+  const elem = document.createElement(type);
+  elem.textContent = text;
+  if (name) {
+    elem.setAttribute("id", name);
+  }
+  for (let s of styles) {
+    elem.classList.add(s);
+  }
+
+  return elem;
+}
+
+//create button
+function createBtn(text, styles, handle, cmd, name) {
+  const btn = createElement("button", text, ["button", ...styles], name);
+  btn.addEventListener("click", () => {
+    handle(cmd);
+  });
+
+  return btn;
+}
+
+// create link
+function createLink(text, linkTo, styles, name) {
+  const link = createElement("a", text, styles, name);
+  link.setAttribute("href", linkTo);
+
+  return link;
+}
+
+//create media
+function createMedia(type, alt, styles, source, show, name) {
+  const media = createElement(type, "", styles, name);
+  media.setAttribute("src", source);
+  media.setAttribute("alt", alt);
+  media.setAttribute("controls", "controls");
+  if (show) {
+    media.setAttribute("encrypted-media");
+  }
+
+  return media;
+}
+
+//make element container
+function createContainer(type, text, styles, comps, name) {
+  const elem = createElement(type, text, styles, name);
+  addToTag(elem, comps, false);
+
+  return elem;
+}
+
+//button with elements inside
+function createButtonContainer(styles, comps, handle, cmd, name) {
+  const btn = createBtn("", styles, handle, cmd, name);
+  addToTag(btn, comps);
+
+  return btn;
+}
+
+//link with elements inside
+function createLinkContainer(styles, comps, text, linkTo, name) {
+  const link = createLink(text, linkTo, styles, name);
+  addToTag(link, comps, false);
+
+  return link;
+}
+//add children to html element, if clear is true remove all elements in tag
+function addToTag(tag, comps, clear) {
+  if (clear) {
+    tag.innerHTML = "";
+  }
+  for (let c of comps) {
+    tag.appendChild(c);
+  }
+}
+
+export {
+  createElement,
+  createBtn,
+  createLink,
+  createMedia,
+  addToTag,
+  createContainer,
+  createButtonContainer,
+  createLinkContainer,
+  addChosen,
+  setChosen,
+};
+
+//-------------utility
+
+//add chosen option to selectors
+function addChosen(selectors) {
+  const chosenSelectors = selectors.map((s) => {
+    return { ...s, chosen: false };
+  });
+  return chosenSelectors;
+}
+
+//set chosen selectors
+function setChosen(selected, selectors) {
+  const chosenSelectors = selectors.map((s) => {
+    return s.title === selected
+      ? { ...s, chosen: true }
+      : { ...s, chosen: false };
+  });
+  return chosenSelectors;
+}
