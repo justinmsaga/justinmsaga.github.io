5020bae45dd44ef98ff6a1cf1972f238e6b72634
Justin Msaga <146983172+justinmsaga@users.noreply.github.com>
2025-12-18 13:35:40 -0800

    button functionality and display selected artefact

--- past/script.js
+++ past/script.js
@@ -1,20 +1,64 @@
 import { crate } from "./archive.js";
-import { addToTag, createButtonContainer, createElement } from "../helper.js";
+import {
+  addToTag,
+  createButtonContainer,
+  createContainer,
+  createElement,
+} from "../helper.js";
 
 //page navigation section
 const nav = document.querySelector(".pageNav");
+const display = document.querySelector(".display");
 
-//create buttons from crate
+//initialize buttons from crate
 const btns = crate.map((c) => {
+  return artifactSelectBtn(c);
+});
+
+//add initial buttons to navigation section
+addToTag(nav, btns);
+
+//create button from artifact
+function artifactSelectBtn(art) {
   //button contents
   const btnText = [
-    createElement("h2", c.title, []),
-    createElement("p", c.craft, []),
+    createElement("h2", art.title, []),
+    createElement("p", art.craft, []),
   ];
 
   //button with contents
-  return createButtonContainer([], btnText, "", "");
-});
+  return createButtonContainer([], btnText, updateNav, art.title);
+}
 
-//add buttons to navingation section
-addToTag(nav, btns, false);
+//setup selected artifact section
+function selectedDisplay(art) {
+  const innerText = [
+    createElement("h2", art.title, []),
+    createElement("p", art.year, []),
+    createElement("p", art.description, []),
+    createElement("p", art.craft, ["selectedCraft"]),
+  ];
+
+  return createContainer("section", "", ["selectedSection"], innerText);
+}
+
+//update navigation section after artifact is selected
+function updateNav(selected) {
+  //display unselected artifact buttons
+  const newBtns = crate.filter((c) => c.title != selected);
+  const btnSection = newBtns.map((c) => {
+    return artifactSelectBtn(c);
+  });
+
+  //display selected artifact
+  const selectedArtifact = crate.find((c) => c.title === selected);
+  const selectedSection = selectedDisplay(selectedArtifact);
+  addToTag(
+    nav,
+    [
+      createContainer("section", "", ["btnSection"], btnSection),
+      selectedSection,
+    ],
+    true
+  );
+}
